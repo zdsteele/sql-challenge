2abf3398b12c21e804f14cd8ea8845f2094280d1
zach steele <zacharysteele8@gmail.com>
2019-09-24 20:07:22 -0500

    pushing

--- solving_problems.sql
+++ solving_problems.sql
@@ -48,5 +48,5 @@ where dept_name = 'Sales' or dept_name = 'Development';
 -- 8. In ascending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 select last_name, count(last_name) from employees 
 group by last_name 
-order by count(last_name) DESC;
+order by count(last_name) ASC;
 
